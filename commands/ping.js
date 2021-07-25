b1ff7848bf3e98b61cbd520aa4d583e59fc6bb43
Vishal Menon <74124373+vmeno0020@users.noreply.github.com>
2021-07-25 14:28:14 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args){
-        message.channel.send('https://www.youtube.com/watch?v=pqBEL-YfcZI');
+        message.channel.send('pong!');
     }
 }
