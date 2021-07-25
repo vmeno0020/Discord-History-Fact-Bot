6af41c4e630cf5138a7144baf02a063e8e4a297e
Vishal Menon <74124373+vmeno0020@users.noreply.github.com>
2021-07-25 14:27:51 -0700

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'Cram',
     description: "sends cramming material",
     execute(message, args){
-        message.channel.send('pong!');
+        message.channel.send('https://www.youtube.com/watch?v=pqBEL-YfcZI');
     }
 }
